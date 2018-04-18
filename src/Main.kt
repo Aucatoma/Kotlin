62945a38b1a07e202085911fed96525170f6b5c6
Daniel Aucatoma <35275582+Aucatoma@users.noreply.github.com>
2018-04-18 09:54:05 -0500

    Hola mundo en Kotlin con Suma de números

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,8 @@
+fun sumarNumeros(numeroUno: Int, numeroDos: Int): Int{
+    return numeroUno + numeroDos
+}
+
 fun main (args: Array<String>){
     println("Hello, world")
+    println(sumarNumeros(2, 3))
 }

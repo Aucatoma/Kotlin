6bd81b0d8c3f7dff4dd375a1776ce6e55592362e
Daniel Aucatoma <35275582+Aucatoma@users.noreply.github.com>
2018-04-18 10:59:20 -0500

    Hola mundo en Kotlin con Suma de números y Tipo de variables, uso de clases, override, etc.

--- src/Main.kt
+++ src/Main.kt
@@ -30,21 +30,36 @@ fun main(args: Array<String>) {
 
     /* Template String */
     println("Superherore: $superHeroe")
+
+    var Juan = Persona("Juan", Integer(12))
+    println(Juan)
+
+    var daniel = PersonaDos(Integer(21), "Daniel", "1717985848")
+    println(daniel)
+
+    var cuadrado1 = Cuadrado("Cuadrado rojo", Integer(12))
+    println(cuadrado1)
 }
 
 
 class Persona {
 
+    lateinit var nombre: String
     lateinit var edadActual: Integer
 
     //constructor(edadActual: Any)
-    constructor(edadActual: Integer) {
+    constructor(nombre: String, edadActual: Integer) {
+        this.nombre = nombre
         this.edadActual = edadActual
     //    iniciar(edad)
     }
 
     fun inciar(edad: Integer){
         edadActual = edad
+    }
+
+    override fun toString(): String {
+        return "El nombre es $nombre, la edad actual es $edadActual"
     }
 }
 
@@ -55,5 +70,30 @@ class PersonaDos(edad: Integer, nombre: String){
 
     constructor(edad2: Integer, nombre2: String, cedula: String) : this(edad2, nombre2){
         nCedula = cedula
+    }
+
+    /* Código que se ejecuta después del constructor */
+    init {
+        iniciar(edad)
+    }
+
+    fun iniciar(edad: Integer){
+        nEdad = edad
+    }
+
+    override fun toString(): String {
+        return "$nNombre tiene $nEdad y su cédula es $nCedula"
+    }
+}
+
+open class Figura(nombre: String){
+    val nNombre = nombre
+}
+
+class Cuadrado(nombre: String, valorLado: Integer): Figura(nombre){
+    var nValorLado = valorLado
+
+    override fun toString(): String {
+        return "El valor del lado del cuadrado es $nValorLado"
     }
 }
